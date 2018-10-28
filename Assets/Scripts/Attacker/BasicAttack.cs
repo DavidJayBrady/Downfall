add25f60c9f800a15136f010b466482cfcf2586d
David Brady <djbrady@uci.edu>
2018-10-27 19:08:27 -0700

    initial attacking in

--- Assets/Scripts/Attacker/BasicAttack.cs
+++ Assets/Scripts/Attacker/BasicAttack.cs
@@ -4,42 +4,56 @@ using UnityEngine;
 
 public class BasicAttack : MonoBehaviour
 {
-    private ControllerMovement _controllerMovement;
-    private BoxCollider2D _boxCollider2D;
+
+    private Vector2 _lookVector;
+
+    public int damage;
     public Vector2 attackVector = new Vector2(-1.0f, -1.0f);
     public LayerMask layerMaskOption;
 
     // Start is called before the first frame update
     void Start()
     {
-        _controllerMovement = GetComponent<ControllerMovement>();
-        _boxCollider2D = GetComponent<BoxCollider2D>();
+        damage = 5;
     }
 
-    // Update is called once per frame
+    // Update is called once     per frame
     void Update()
     {
 
+    }
+
+    public void Attack()
+    {
+        UpdateLookVector();
+        RaycastHit2D hit = FindObjectToHit();
+        HitObject(hit);
+    }
+
+
+
+    void UpdateLookVector()
+    {
         Vector2 lastMoveAttempt = Common.GetScaledVectorInput("Horizontal", "Vertical");
-        Common.VectorNormalize(ref lastMoveAttempt);
+        if (lastMoveAttempt.magnitude > .5f)
+        {
+            _lookVector = lastMoveAttempt;
+        }
+    }
 
-//        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, lastMoveAttempt, layerMask);
-//        if (hit.collider != null)
-//        {
-//            Debug.Log(hit.ToString());
-//            Debug.Log(hit.collider.transform.position);
-//        }
+    RaycastHit2D FindObjectToHit()
+    {
+        return Physics2D.Raycast(origin: transform.position,
+                                    direction: _lookVector,
+                                    distance: Mathf.Sqrt(3),
+                                    layerMask: layerMaskOption);
+    }
 
-        RaycastHit2D hit = Physics2D.Raycast(origin: transform.position,
-                                               direction:lastMoveAttempt,
-                                                distance: Mathf.Infinity,
-                                                 layerMask: layerMaskOption);
+    void HitObject(RaycastHit2D hit)
+    {
         if (hit.collider != null)
         {
-            Debug.Log(hit.ToString());
-            Debug.Log(hit.collider.transform.position);
+            hit.collider.GetComponent<Building>().Damage(damage);
         }
-
-
     }
 }
