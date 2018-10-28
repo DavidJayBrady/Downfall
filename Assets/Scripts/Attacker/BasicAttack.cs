4c2589635cdeddd83cf57c92170f94e6490a6203
David Brady <djbrady@uci.edu>
2018-10-27 17:25:03 -0700

    raycast working with layers

--- Assets/Scripts/Attacker/BasicAttack.cs
+++ Assets/Scripts/Attacker/BasicAttack.cs
@@ -7,7 +7,7 @@ public class BasicAttack : MonoBehaviour
     private ControllerMovement _controllerMovement;
     private BoxCollider2D _boxCollider2D;
     public Vector2 attackVector = new Vector2(-1.0f, -1.0f);
-    public LayerMask layerMask;
+    public LayerMask layerMaskOption;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +23,23 @@ public class BasicAttack : MonoBehaviour
         Vector2 lastMoveAttempt = Common.GetScaledVectorInput("Horizontal", "Vertical");
         Common.VectorNormalize(ref lastMoveAttempt);
 
-        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position - (3f*_boxCollider2D.offset), lastMoveAttempt, layerMask);
+//        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, lastMoveAttempt, layerMask);
+//        if (hit.collider != null)
+//        {
+//            Debug.Log(hit.ToString());
+//            Debug.Log(hit.collider.transform.position);
+//        }
+
+        RaycastHit2D hit = Physics2D.Raycast(origin: transform.position,
+                                               direction:lastMoveAttempt,
+                                                distance: Mathf.Infinity,
+                                                 layerMask: layerMaskOption);
         if (hit.collider != null)
         {
             Debug.Log(hit.ToString());
             Debug.Log(hit.collider.transform.position);
         }
+
 
     }
 }
