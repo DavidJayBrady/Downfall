7b30be1b5b9febd7e07a27983ecedfb7c537eb54
David Brady <djbrady@uci.edu>
2018-10-27 17:08:21 -0700

    raycast almost done. still detecting attacker. will add layers. made folder for attacker scripts

--- Assets/Scripts/Attacker/BasicAttack.cs
+++ Assets/Scripts/Attacker/BasicAttack.cs
@@ -5,30 +5,30 @@ using UnityEngine;
 public class BasicAttack : MonoBehaviour
 {
     private ControllerMovement _controllerMovement;
+    private BoxCollider2D _boxCollider2D;
     public Vector2 attackVector = new Vector2(-1.0f, -1.0f);
+    public LayerMask layerMask;
 
     // Start is called before the first frame update
     void Start()
     {
         _controllerMovement = GetComponent<ControllerMovement>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 1. find location to end at
-        //     a. scale lookVector to gridSpace
-        //     b.
+
         Vector2 lastMoveAttempt = Common.GetScaledVectorInput("Horizontal", "Vertical");
-        if (lastMoveAttempt.magnitude > 0.5f)
+        Common.VectorNormalize(ref lastMoveAttempt);
+
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position - (3f*_boxCollider2D.offset), lastMoveAttempt, layerMask);
+        if (hit.collider != null)
         {
-            Common.VectorNormalize(ref lastMoveAttempt);
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),
-                out hit, lastMoveAttempt.magnitude))
-            {
-                Debug.Log("hit");
-            }
+            Debug.Log(hit.ToString());
+            Debug.Log(hit.collider.transform.position);
         }
+
     }
 }
