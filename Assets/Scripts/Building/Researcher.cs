0e029d577e08e3fe8aaf90e20d2018b28de0800b
chen-ethan <ethanchen111@gmail.com>
2018-11-06 15:46:22 -0800

    barely started research code

--- Assets/Scripts/Building/Researcher.cs
+++ Assets/Scripts/Building/Researcher.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(Building))]
 public class Researcher : MonoBehaviour
 {
+
+    public MatchManager matchManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        matchManager.matchTimeStart = true;
     }
 
     // Update is called once per frame
